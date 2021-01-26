2771d2fa9505b89a2e4995a211b440851efcde05
KrishnaJaykrishna <67735372+KrishnaJaykrishna@users.noreply.github.com>
2021-01-26 12:50:11 +0530

    Add files via upload

---
+++ median.py
@@ -0,0 +1,19 @@
+import csv
+with open('HeightWeight.csv', newline = '') as f:
+    reader = csv.reader(f)
+    file_data = list(reader)
+file_data.pop(0)
+newdata = []
+for i in range(len(file_data)):
+    numb = file_data[i][1]
+    newdata.append(float(numb))
+n = len(newdata)
+newdata.sort()
+if n % 2 == 0:
+    median1 = float(newdata[n//2])
+    median2 = float(newdata[n//2 - 1])
+    median = (median2 + median1)/2
+else:
+    median = newdata[n//2]
+print(median)
+print (n)
---
+++ mode.py
@@ -0,0 +1,35 @@
+import csv
+from collections import Counter
+with open('HeightWeight.csv', newline = '') as f:
+    reader = csv.reader(f)
+    file_data = list(reader)
+file_data.pop(0)
+newdata = []
+for i in range(len(file_data)):
+    numb = file_data[i][1]
+    newdata.append(float(numb))
+n = len(newdata)
+data = Counter(newdata)
+get_mode = dict(data)
+mode = []
+mode1 = []
+mode2 = []
+for a,v in get_mode.items():
+    a = float(a)
+    if 50<a<60 :
+        if v == max(list(data.values())):
+            mode.append(a)
+    elif 60<a<70 :
+        if v == max(list(data.values())):
+            mode1.append(a)
+    elif 70<a<75 :
+        if v == max(list(data.values())):
+            mode2.append(a)
+print (len(mode), len(mode2), len(mode1))
+if len(mode)>len(mode1) and len(mode2):
+    print (mode)
+elif len(mode1)>len(mode2) and len(mode):
+    print (mode1)
+    print (True)
+elif len(mode2)>len(mode1) and len(mode):
+    print (mode2)
